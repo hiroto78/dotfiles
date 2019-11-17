aee6d4ab8184441bc814143925ad573c9ed00453
Hiroto Naya <hiroto.naya.78@gmail.com>
2019-11-17 19:21:56 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,6 @@ ln -sf /${ROOT}/${USER}/repos/dotfiles/git/.gitconfig /${ROOT}/${USER}/.gitconfi
 ln -sf /${ROOT}/${USER}/repos/dotfiles/tmux/.tmux.conf /${ROOT}/${USER}/.tmux.conf
 ln -sf /${ROOT}/${USER}/repos/dotfiles/vim/.vimrc /${ROOT}/${USER}/.vimrc
 ln -sf /${ROOT}/${USER}/repos/dotfiles/zsh/.zshrc /${ROOT}/${USER}/.zshrc
+
+ln -sf /${ROOT}/${USER}/repos/dotfiles/vscode/settings.json /${ROOT}/${USER}/Library/Application\ Support/Code/User/settings.json
+ln -sf /${ROOT}/${USER}/repos/dotfiles/vscode/keybindings.json /${ROOT}/${USER}/Library/Application\ Support/Code/User/keybindings.json
